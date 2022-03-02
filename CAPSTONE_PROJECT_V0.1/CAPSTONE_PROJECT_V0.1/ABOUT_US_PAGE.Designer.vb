644db7ef24315af06d97221c1b544f521960e145
JeffLLake <97986163+JeffLLake@users.noreply.github.com>
2022-03-02 12:53:36 -0500

    Add files via upload
    Added the ability to return to menu page from about us and products page. Also added the buttons for a few products and their labels.

--- CAPSTONE_PROJECT_V0.1/CAPSTONE_PROJECT_V0.1/ABOUT_US_PAGE.Designer.vb
+++ CAPSTONE_PROJECT_V0.1/CAPSTONE_PROJECT_V0.1/ABOUT_US_PAGE.Designer.vb
@@ -22,16 +22,42 @@ Partial Class ABOUT_US_PAGE
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Return_Menu_Button = New System.Windows.Forms.Button()
+        Me.Return_Menu_Label = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'ABOUT_US
+        'Return_Menu_Button
+        '
+        Me.Return_Menu_Button.Location = New System.Drawing.Point(12, 32)
+        Me.Return_Menu_Button.Name = "Return_Menu_Button"
+        Me.Return_Menu_Button.Size = New System.Drawing.Size(111, 29)
+        Me.Return_Menu_Button.TabIndex = 0
+        Me.Return_Menu_Button.Text = "Enter"
+        Me.Return_Menu_Button.UseVisualStyleBackColor = True
+        '
+        'Return_Menu_Label
+        '
+        Me.Return_Menu_Label.AutoSize = True
+        Me.Return_Menu_Label.Location = New System.Drawing.Point(12, 9)
+        Me.Return_Menu_Label.Name = "Return_Menu_Label"
+        Me.Return_Menu_Label.Size = New System.Drawing.Size(111, 20)
+        Me.Return_Menu_Label.TabIndex = 1
+        Me.Return_Menu_Label.Text = "Return to Menu"
+        '
+        'ABOUT_US_PAGE
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Name = "ABOUT_US"
+        Me.Controls.Add(Me.Return_Menu_Label)
+        Me.Controls.Add(Me.Return_Menu_Button)
+        Me.Name = "ABOUT_US_PAGE"
         Me.Text = "About Us Page"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents Return_Menu_Button As Button
+    Friend WithEvents Return_Menu_Label As Label
 End Class
--- CAPSTONE_PROJECT_V0.1/CAPSTONE_PROJECT_V0.1/ABOUT_US_PAGE.vb
+++ CAPSTONE_PROJECT_V0.1/CAPSTONE_PROJECT_V0.1/ABOUT_US_PAGE.vb
@@ -1,3 +1,8 @@
 ﻿Public Class ABOUT_US_PAGE
+    Private Sub Return_Menu_Button_Click(sender As Object, e As EventArgs) Handles Return_Menu_Button.Click
+        Dim ABOUT_US_PAGE As New TITLE_PAGE_MENU
 
+        Hide()
+        ABOUT_US_PAGE.ShowDialog()
+    End Sub
 End Class
