0bac8cb8428498ebf642ce451e6cc641ae64e8ae
JeffLLake <97986163+JeffLLake@users.noreply.github.com>
2022-03-05 16:09:17 -0500

    Add files via upload

---
+++ CAPSTONE_PROJECT_V0.1/CAPSTONE_PROJECT_V0.1/CART_PAGE.vb
@@ -0,0 +1,12 @@
+﻿Public Class CART_PAGE
+    Private Sub Return_Product_Page_Button_Click(sender As Object, e As EventArgs) Handles Return_Product_Page_Button.Click
+        Dim zone As New PRODUCTS_PAGE
+
+        Hide()
+        zone.ShowDialog()
+    End Sub
+
+    Private Sub CART_PAGE_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
